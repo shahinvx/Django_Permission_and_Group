50842334d18f21d1fbcdf82c1dcedfdb654fc15e
Md. Mahmudul Hasan Shahin <shahinvx@gmail.com>
2021-11-09 01:14:59 +0600

    Update settings.py

--- root_app/settings.py
+++ root_app/settings.py
@@ -137,6 +137,6 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
