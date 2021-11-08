e13d5bd7211d9874b2d769f05e793a46078c8eaf
Md. Mahmudul Hasan Shahin <shahinvx@gmail.com>
2021-11-09 00:11:07 +0600

    Update settings.py

--- root_app/settings.py
+++ root_app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9x$delvm=h2l*yta$3p=%taiv^u%%wd%px^fm8sk9mk)j@m(td'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-perm.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
